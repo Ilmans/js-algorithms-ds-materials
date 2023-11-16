44a4baa0db16734b0235e39de2423473068fbe46
Ilman Sunanuddin <ilmansunannudin2@gmail.com>
2023-11-16 10:20:49 +0700

    insertion & quick sort

--- algorithms/sort.js
+++ algorithms/sort.js
@@ -1,21 +1,61 @@
+
+
+// big o =  O(n2)
 export const bubbleSort = (arr) => {
+  let n = arr.length;
+  let swapped;
 
-    let n = arr.length;
-    let swapped;
- 
-    do {
-        swapped = false;
-        for (let i = 0;i < n; i++)
-        {
-          
-            if(arr[i] > arr[ i + 1 ]){
-                [arr[i],arr[i+1]] = [arr[i+1],arr[i]];
-                swapped = true;
-            }
+  do {
+    swapped = false;
+    for (let i = 0; i < n; i++) {
+      if (arr[i] > arr[i + 1]) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        swapped = true;
+      }
+    }
+
+    n--;
+  } while (swapped);
+
+  return arr;
+};
+
+
+// big o =  O(n2)
+export const insertionSort = (arr) => {
+  let n = arr.length;
+
+  for (let i = 1; i < n; i++) {
+    let currentElement = arr[i];
+    let j = i - 1;
+
+    while (j >= 0 && arr[j] > currentElement) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+
+    arr[j + 1] = currentElement;
+  }
+
+  return arr;
+};
+
+
+// big o = O(log n)
+export const quickSort = (arr) => {
+
+    if(arr.length <= 1) return arr;
+    let pivot = arr[0];
+    let left = [];
+    let right = [];
+
+    for ( let i = 1; i < arr.length; i ++) {
+        if(arr[i] < pivot){
+            left.push(arr[i])
+        } else {
+            right.push(arr[i])
         }
+    }
 
-        n--
-    } while (swapped)
-
-    return arr;
+    return quickSort(left).concat(pivot, quickSort(right));
 }
--- test/sortTest.js
+++ test/sortTest.js
@@ -1,6 +1,6 @@
 import assert from "assert";
 import { describe, it } from "./test.js";
-import { bubbleSort } from "../algorithms/sort.js";
+import { bubbleSort, insertionSort, quickSort } from "../algorithms/sort.js";
 
 export const sortTest = () => {
     describe("Test bubble sort", () => {
@@ -22,6 +22,53 @@ export const sortTest = () => {
   
       it("should sort an empty array", () => {
         assert.deepStrictEqual(bubbleSort([]), []);
+      });
+    });
+
+
+
+    describe("Test insertion sort", () => {
+      it("should sort [1, 3, 4, 1, 5, 9, 2, 6, 5, 3, 5]", () => {
+        assert.deepStrictEqual(insertionSort([1, 3, 4, 1, 5, 9, 2, 6, 5, 3, 5]), [1, 1, 2, 3, 3, 4, 5, 5, 5, 6, 9]);
+      });
+  
+      it("should sort [9, 8, 7, 6, 5, 4, 3, 2, 1]", () => {
+        assert.deepStrictEqual(insertionSort([9, 8, 7, 6, 5, 4, 3, 2, 1]), [1, 2, 3, 4, 5, 6, 7, 8, 9]);
+      });
+  
+      it("should sort [5, 5, 5, 5, 5]", () => {
+        assert.deepStrictEqual(insertionSort([5, 5, 5, 5, 5]), [5, 5, 5, 5, 5]);
+      });
+  
+      it("should sort [1]", () => {
+        assert.deepStrictEqual(insertionSort([1]), [1]);
+      });
+  
+      it("should sort an empty array", () => {
+        assert.deepStrictEqual(insertionSort([]), []);
+      });
+    });
+  
+
+    describe("Test quick sort", () => {
+      it("should sort [1, 3, 4, 1, 5, 9, 2, 6, 5, 3, 5]", () => {
+        assert.deepStrictEqual(quickSort([1, 3, 4, 1, 5, 9, 2, 6, 5, 3, 5]), [1, 1, 2, 3, 3, 4, 5, 5, 5, 6, 9]);
+      });
+  
+      it("should sort [9, 8, 7, 6, 5, 4, 3, 2, 1]", () => {
+        assert.deepStrictEqual(quickSort([9, 8, 7, 6, 5, 4, 3, 2, 1]), [1, 2, 3, 4, 5, 6, 7, 8, 9]);
+      });
+  
+      it("should sort [5, 5, 5, 5, 5]", () => {
+        assert.deepStrictEqual(quickSort([5, 5, 5, 5, 5]), [5, 5, 5, 5, 5]);
+      });
+  
+      it("should sort [1]", () => {
+        assert.deepStrictEqual(quickSort([1]), [1]);
+      });
+  
+      it("should sort an empty array", () => {
+        assert.deepStrictEqual(quickSort([]), []);
       });
     });
   };
