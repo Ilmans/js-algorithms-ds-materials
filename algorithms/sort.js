8429b70353e30e1c734ed6ffd39b70816fa46e75
Ilman Sunanuddin <ilmansunannudin2@gmail.com>
2023-11-17 10:06:55 +0700

    merge sort

--- algorithms/sort.js
+++ algorithms/sort.js
@@ -1,5 +1,3 @@
-
-
 // big o =  O(n2)
 export const bubbleSort = (arr) => {
   let n = arr.length;
@@ -20,7 +18,6 @@ export const bubbleSort = (arr) => {
   return arr;
 };
 
-
 // big o =  O(n2)
 export const insertionSort = (arr) => {
   let n = arr.length;
@@ -40,22 +37,54 @@ export const insertionSort = (arr) => {
   return arr;
 };
 
-
 // big o = O(log n)
 export const quickSort = (arr) => {
+  if (arr.length <= 1) return arr;
+  let pivot = arr[0];
+  let left = [];
+  let right = [];
 
-    if(arr.length <= 1) return arr;
-    let pivot = arr[0];
-    let left = [];
-    let right = [];
-
-    for ( let i = 1; i < arr.length; i ++) {
-        if(arr[i] < pivot){
-            left.push(arr[i])
-        } else {
-            right.push(arr[i])
-        }
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
     }
+  }
 
-    return quickSort(left).concat(pivot, quickSort(right));
+  return quickSort(left).concat(pivot, quickSort(right));
+};
+
+// BIG O O(n log n)
+export function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  const middle = Math.floor(arr.length / 2);
+
+  const left = arr.slice(0, middle);
+  const right = arr.slice(middle);
+
+  const leftSorted = mergeSort(left);
+  const rightSorted = mergeSort(right);
+
+  return merge(leftSorted, rightSorted);
+}
+
+function merge(left, right) {
+  let result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      result.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
 }
